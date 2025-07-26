6218b4ccfb6a5a4583d8b78d26734eb2feda3725
Eloon Ing <eing067@aucklanduni.ac.nz>
2025-07-27 00:36:20 +1200

    added functinal buttons instead of mockup buttons from daisyUI

--- app/components/Mainwindow.jsx
+++ app/components/Mainwindow.jsx
@@ -1,7 +1,19 @@
 function Mainwindow() {
     return(
-        <div className="mockup-window bg-base-200 border border-white outline-2 rounded-xl w-200 h-150 mx-auto top-90">
-            <div className="grid place-content-center border-t border-white outline h-150">Hello!</div>
+        <div className="mockup-browser bg-base-200 border border-white outline-2 rounded-xl w-200 h-150 mx-auto top-90">
+            <div className="flex items-center px-4 py-2 border-b border-white relative">
+                <div className="flex space-x-2">
+                    <button className="w-3 h-3 rounded-full bg-red-500"/>
+                    <button className="w-3 h-3 rounded-full bg-yellow-400"/>
+                    <button className="w-3 h-3 rounded-full bg-green-500"/>
+                </div>
+                <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-sm">Home</h1>
+            </div>
+
+            <div className="flex flex-col items-center border-t border-white outline h-150">
+                <h1>Name</h1>
+                <h1>hi</h1>
+            </div>
         </div>
     )
 }
